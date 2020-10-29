9d4f3062664567d889a98023d62c2a5bedddef3e
Imants Pulkstenis <imants.pulkstenis@protonmail.com>
2020-10-29 20:59:37 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # VGA_Ambilight
-Final project of RTR710 @2020/2021
+The final project of RTR710 @2020/2021
---
+++ source/main.vhd
@@ -0,0 +1,38 @@
+-----------------------------
+-- Author - Imants Pulkstenis
+-- Date - 29.10.2020 
+-- Project name - VGA_Ambilight  
+-- Module name - main - TOP entity
+--
+-- Detailed module description
+-- Top module
+--
+-- Revision:
+-- A - initial design
+-- B - 
+--
+-----------------------------
+LIBRARY ieee; --always use this library
+USE ieee.std_logic_1164.ALL; --always use this library
+USE ieee.numeric_std.ALL; --use this library if arithmetic require
+ENTITY main IS
+    PORT (
+        i_clk : IN STD_LOGIC
+    );
+END main;
+
+ARCHITECTURE rtl OF main IS
+    -- signal declaration
+    -- <your code goes here>
+
+BEGIN
+    -- reg-state logic
+    -- <your code goes here>
+
+    -- next-state logic
+    -- <your code goes here>
+
+    -- outputs
+    -- <your code goes here>
+
+END rtl;
